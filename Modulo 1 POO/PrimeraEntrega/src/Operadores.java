15418895e2220931e37893483858cd9c40be8684
alonso1298 <alonsosagrerogranados@gmail.com>
2025-09-26 20:46:31 -0600

    Operadores

---
+++ Modulo 1 POO/PrimeraEntrega/src/Operadores.java
@@ -0,0 +1,13 @@
+public class Operadores {
+    public static void main(String[] args) {
+        System.out.println("Operadores aritmeticos");
+        int numero = 100;
+        int otro = 11;
+
+        System.out.println(numero + otro); // Suma
+        System.out.println(numero - otro); // Rest
+        System.out.println(numero * otro); // Multiplication
+        System.out.println(numero / otro); // Division
+        System.out.println(numero % otro); // Residuo de dividir un numero entre otro
+    }
+}
