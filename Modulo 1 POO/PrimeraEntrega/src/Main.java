a1f1d66f4bdc384eedb4efc25affbcaf58f8d0a7
alonso1298 <alonsosagrerogranados@gmail.com>
2025-09-26 18:04:12 -0600

    Primer Commit

---
+++ Modulo 1 POO/PrimeraEntrega/src/Main.java
@@ -0,0 +1,7 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main { // Comienza la definicion de una clase
+    public static void main(String[] args) { // Metodo principal
+        System.out.println("Hola mundo"); // Despliega en pantalla "Hola mundo"
+    }
+}
