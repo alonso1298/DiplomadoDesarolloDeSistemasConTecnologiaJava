4b5ad723d07e7a0894eafe4c30692b0d07f5f1cd
alonso1298 <alonsosagrerogranados@gmail.com>
2025-09-27 10:30:20 -0600

    OperadoresBits

---
+++ Modulo 1 POO/PrimeraEntrega/src/OperadoresBit.java
@@ -0,0 +1,8 @@
+public class OperadoresBit {
+    public static void main(String[] args) {
+        System.out.println("Operadores a nivel de bit");
+        System.out.println("Logica de Bits");
+        int unaHamburguesa = 255;
+        System.out.println(unaHamburguesa & 4);
+    }
+}
