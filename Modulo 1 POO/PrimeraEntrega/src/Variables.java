7d1c4a4b1f5550456cd889bcd038f35fecf08183
alonso1298 <alonsosagrerogranados@gmail.com>
2025-09-26 18:20:13 -0600

    Segundo programa variables

---
+++ Modulo 1 POO/PrimeraEntrega/src/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Definicion de variables
+        int numero;
+
+        // Definicion e inicializar variables
+        int edad = 99;
+
+        // Uso de variable
+        System.out.println(edad);
+
+        // System.out.println(numero); ERROR por lo que las variables: 1. Se define
+    }
+}
