f8df33e05cddab6b6ca21f2e94f3cf3d5e1c072a
alonso1298 <alonsosagrerogranados@gmail.com>
2025-10-03 16:21:55 -0600

    Se agrega funcion

--- Modulo 1 POO/PrimeraEntrega/src/Funciones.java
+++ Modulo 1 POO/PrimeraEntrega/src/Funciones.java
@@ -4,10 +4,17 @@ public class Funciones {
         return 10;
     }
 
+    public static double suma (int num1, int num2) {
+        double resultado = 0;
+        return resultado = num1 + num2;
+    }
+
     public static void main(String[] args) {
         System.out.println("Funciones");
         System.out.println(siempre10()); // Llamado a la funcion
         int variable = siempre10();
         System.out.println(variable);
+
+        System.out.println("Resultado de llamar a suma: " + suma(5,4));
     }
 }
