802e9519ba5de0baa56ef48d68c34b2e9df354ac
alonso1298 <alonsosagrerogranados@gmail.com>
2025-10-04 10:47:13 -0600

    Primera clase y primera instanciacion

---
+++ Modulo 1 POO/PrimeraEntrega/src/Casa.java
@@ -0,0 +1,11 @@
+public class Casa {
+    // Atrubutos (Variables)
+    // Tamaño del terrno, metros de construccion, material, numero de pisos, numero de cuartos, color, direccion, precio, dueño
+    public double precio;
+    public String propietario;
+    public String color;
+    public int pisos;
+    int ventanas;
+
+    // Metodos (Procedimientos y Funciones)
+}
---
+++ Modulo 1 POO/PrimeraEntrega/src/PruebaInstanciacion.java
@@ -0,0 +1,31 @@
+public class PruebaInstanciacion {
+    public static void main(String[] args) {
+        System.out.println("Ejemplo de creacion de objetos (instanciacion)");
+
+        // Creacion de objetos
+        // Clase objeto = new Clase();
+        // Si quiero crear un objeto llamado casaMauricio
+        Casa casaMauricio = new Casa();
+        Casa nuevaCasa = new Casa();
+
+        // Para hecer uso de un atributo de un objeto usaremos:
+        // nombre del objeto.nombre del atruibuto
+        casaMauricio.pisos = 3;
+        casaMauricio.ventanas = 10;
+        casaMauricio.precio = 100;
+        casaMauricio.color = "Rojo";
+
+        System.out.println("Casa de Mauricio pisos: " + casaMauricio.pisos);
+        System.out.println("Casa de Mauricio precio: $ " + casaMauricio.precio);
+        System.out.println("Casa de Mauricio ventanas: " + casaMauricio.ventanas);
+        System.out.println("Casa de Mauricio color: " + casaMauricio.color);
+
+        System.out.println("\n");
+
+        nuevaCasa = casaMauricio;
+        System.out.println("Casa de Nueva pisos: " + casaMauricio.pisos);
+        System.out.println("Casa de Nueva precio: $ " + casaMauricio.precio);
+        System.out.println("Casa de Nueva ventanas: " + casaMauricio.ventanas);
+        System.out.println("Casa de Nueva color: " + casaMauricio.color);
+    }
+}
