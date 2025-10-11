a4cd01389ef866bc4109e994743873d96c4f0346
alonso1298 <alonsosagrerogranados@gmail.com>
2025-10-11 11:47:52 -0600

    Refactor method 2

--- Modulo 1 POO/SegundaEntrega/src/Radio.java
+++ Modulo 1 POO/SegundaEntrega/src/Radio.java
@@ -1,6 +1,6 @@
 public class Radio  extends AparatoElectronico {
     @Override
-    public int bajaVolumen() { // Metodo que es abstract en la super clase
+    public int bajaVolumen() { // Metodo que es abstract en la super clase AparatoElectronico
         this.volumen--;
         return this.volumen;
     }
