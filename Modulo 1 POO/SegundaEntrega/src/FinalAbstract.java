766ad717b427541fa8cd0d0b4dd03b260748d131
alonso1298 <alonsosagrerogranados@gmail.com>
2025-10-11 12:02:10 -0600

    add variable final

--- Modulo 1 POO/SegundaEntrega/src/FinalAbstract.java
+++ Modulo 1 POO/SegundaEntrega/src/FinalAbstract.java
@@ -1,8 +1,16 @@
 public class FinalAbstract {
     public static void main(String[] args) {
         System.out.println("Final");
-
-        AparatoElectronico objeto = new AparatoElectronico();
+        // Metodos y clases abstract
+        Radio objeto = new Radio();
         objeto.subeVolumen();
+
+        System.out.println("Final");
+        double precio = 10;
+        precio *= 1.13;
+        System.out.println(precio);
+
+        final double IVA = 0.16; // Constante
+        System.out.println("IVA de " + IVA);
     }
 }
