3f011f7406cc28af6aa82fa861caa13b70195404
alonso1298 <alonsosagrerogranados@gmail.com>
2025-10-18 09:23:13 -0600

    add class Libro

---
+++ Modulo 1 POO/SegundaEntrega/src/Libro.java
@@ -0,0 +1,42 @@
+public class Libro {
+    private String titulo;
+    private String autor;
+    private int edicion;
+
+    public Libro() {
+
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getEdicion() {
+        return edicion;
+    }
+
+    public void setEdicion(int edicion) {
+        this.edicion = edicion;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", edicion=" + edicion +
+                '}';
+    }
+}
