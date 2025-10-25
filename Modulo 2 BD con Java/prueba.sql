f7c58f94196f8a46bdbcf8faeac41ee380e75f10
alonso1298 <alonsosagrerogranados@gmail.com>
2025-10-25 11:28:13 -0600

    add comands sql

---
+++ Modulo 2 BD con Java/prueba.sql
@@ -0,0 +1,19 @@
+#Primer ejemplo de BD
+create database prueba character set utf8mb4 collate utf8mb4_unicode_ci;
+
+use prueba;
+
+create table usuario(
+	nombre varchar(100)
+);
+
+#create user curso@localhost IDENTIFIED by "cursos";
+
+alter table usuario add column correo_electronico varchar(100);
+
+drop table usuario;
+
+drop database prueba;
+
+insert into usuario values('JUAN RODRÍGUEZ', 'juan@unam.mx');
+insert into usuario (correo_electronico, nombre) values('JUAN RODRÍGUEZ', 'juan@unam.mx');
