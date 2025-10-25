a3760d9951330d9f35c3a68dbe56ff2138f7ba69
alonso1298 <alonsosagrerogranados@gmail.com>
2025-10-25 11:49:12 -0600

    add comand COUNT

--- Modulo 2 BD con Java/prueba.sql
+++ Modulo 2 BD con Java/prueba.sql
@@ -14,15 +14,24 @@ alter table usuario add column correo_electronico varchar(100);
 drop table usuario;
 
 drop database prueba;
+INSERT INTO usuario VALUES ("Juan rodriguez", "juan@unam.mx");
 
-insert into usuario values('JUAN RODRÍGUEZ', 'juan@unam.mx');
-insert into usuario (correo_electronico, nombre) values('JUAN RODRÍGUEZ', 'juan@unam.mx');
+INSERT INTO usuario (correo_electronico, nombre)
+	VALUES ("manuel@unam.mx", "Manuel Rodriguez");
 
 UPDATE usuario u SET nombre = "Juan Antonio Perez"
 	WHERE u.correo_electronico = "juan@aunam.mx";
 
-DELETE FROM usuario WHERE correo_electronico = "juan@aunam.mx";
+DELETE FROM usuario WHERE correo_electronico = "juan@unam.mx";
+
+ALTER TABLE usuario ADD COLUMN carrera VARCHAR(100);
+
+UPDATE usuario SET carrera = "Matematicas";
 
 SELECT * FROM usuario;
 
 SELECT * FROM usuario ORDER BY nombre DESC;
+
+INSERT INTO usuario VALUES ("Alonso Sagrero", "alonso@unam.mx", "Fisica");
+
+SELECT carrera, COUNT(*) as numero_usuario FROM usuario GROUP BY carrera ORDER BY carrera DESC;
