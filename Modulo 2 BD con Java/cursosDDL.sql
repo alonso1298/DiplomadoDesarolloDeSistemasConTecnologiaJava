095ec041c51e2f283c5efde5410f52c83e2113a4
alonso1298 <alonsosagrerogranados@gmail.com>
2025-10-25 12:31:49 -0600

    create new table

--- Modulo 2 BD con Java/cursosDDL.sql
+++ Modulo 2 BD con Java/cursosDDL.sql
@@ -9,3 +9,12 @@ CREATE TABLE estudiante (
     correo_electronico VARCHAR(50) NOT NULL,
     numero_cuenta VARCHAR(10) NOT NULL UNIQUE
 );
+
+CREATE TABLE instructor(
+	id INT AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    apellido_paterno VARCHAR(50) NOT NULL,
+    correo_electronico VARCHAR(50) NOT NULL,
+    especialidad VARCHAR(50),
+    PRIMARY KEY (id)
+);
