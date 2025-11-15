2893a65ac2f351c428028ebe43fcb2e4e42aafcd
alonso1298 <alonsosagrerogranados@gmail.com>
2025-11-15 09:39:47 -0600

    Refactor

--- Modulo 3 Persistencia con Hibernate/jdbc-ejemplo/src/main/java/com/dgtic/unam/dao/BookDAO.java
+++ Modulo 3 Persistencia con Hibernate/jdbc-ejemplo/src/main/java/com/dgtic/unam/dao/BookDAO.java
@@ -7,26 +7,26 @@ import java.util.List;
 public class BookDAO {
     // SEELECT * WHERE
     public Book findById(String id){
-        throw new UnsupportedOperationException("Not Soported Jet.");
+        throw new UnsupportedOperationException("Not Soported yet.");
     }
 
     // SELECT *
     public List<Book> findAll(){
-        throw new UnsupportedOperationException("Not Soported Jet.");
+        throw new UnsupportedOperationException("Not Soported yet.");
     }
 
     // INSERT
     public void save(){
-        throw new UnsupportedOperationException("Not Soported Jet.");
+        throw new UnsupportedOperationException("Not Soported yet.");
     }
 
     // UPDATE
     public void update(Book book){
-        throw new UnsupportedOperationException("Not Soported Jet.");
+        throw new UnsupportedOperationException("Not Soported yet.");
     }
 
     // DELETE FROM
     public void delete(Book book){
-        throw new UnsupportedOperationException("Not Soported Jet.");
+        throw new UnsupportedOperationException("Not Soported yet.");
     }
 }
