1d4d46592b733e1726355fc01b28fbebbcb17b36
alonso1298 <alonsosagrerogranados@gmail.com>
2025-11-15 11:15:54 -0600

    Add class sala

---
+++ Modulo 3 Persistencia con Hibernate/Ejercicio_1/Ejercicio_1.1-2/ejercicio-1-mod-3-mapeo-cine/src/main/java/mx/unam/dgtic/entities/Sala.java
@@ -0,0 +1,33 @@
+package mx.unam.dgtic.entities;
+
+public class Sala {
+    private int idSala;
+    private String nombre;
+    private int capacidad;
+
+    public Sala(){}
+
+    public int getIdSala() {
+        return idSala;
+    }
+
+    public void setIdSala(int idSala) {
+        this.idSala = idSala;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+}
