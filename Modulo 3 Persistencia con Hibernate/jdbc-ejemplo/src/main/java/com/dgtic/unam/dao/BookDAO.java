d4142c841891365d25c572bad24443783a57b84a
alonso1298 <alonsosagrerogranados@gmail.com>
2025-11-14 21:02:23 -0600

    add methods

---
+++ Modulo 3 Persistencia con Hibernate/jdbc-ejemplo/src/main/java/com/dgtic/unam/dao/BookDAO.java
@@ -0,0 +1,34 @@
+package com.dgtic.unam.dao;
+
+import com.dgtic.unam.model.Book;
+
+import java.util.List;
+
+public class BookDAO {
+    // SEELECT * WHERE
+    public Book findById(String id){
+
+        return null;
+    }
+
+    // SELECT *
+    public List<Book> findAll(){
+
+        return List.of();
+    }
+
+    // INSERT
+    public void save(){
+
+    }
+
+    // UPDATE
+    public void update(Book book){
+
+    }
+
+    // DELETE FROM
+    public void delete(Book book){
+
+    }
+}
