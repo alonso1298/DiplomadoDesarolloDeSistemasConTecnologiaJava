17fd3ae30bddf01428ff46d6d48fd21a09a3e6cb
alonso1298 <alonsosagrerogranados@gmail.com>
2025-11-15 11:11:44 -0600

    Add class boleto

---
+++ Modulo 3 Persistencia con Hibernate/Ejercicio_1/Ejercicio_1.1-2/ejercicio-1-mod-3-mapeo-cine/src/main/java/mx/unam/dgtic/entities/Boleto.java
@@ -0,0 +1,44 @@
+package mx.unam.dgtic.entities;
+
+import java.util.Date;
+
+public class Boleto {
+    private int idBoleto;
+    private String asiento;
+    private Date fechaCompra;
+    private int idFuncion;
+
+    public Boleto(){}
+
+    public int getIdBoleto() {
+        return idBoleto;
+    }
+
+    public void setIdBoleto(int idBoleto) {
+        this.idBoleto = idBoleto;
+    }
+
+    public String getAsiento() {
+        return asiento;
+    }
+
+    public void setAsiento(String asiento) {
+        this.asiento = asiento;
+    }
+
+    public Date getFechaCompra() {
+        return fechaCompra;
+    }
+
+    public void setFechaCompra(Date fechaCompra) {
+        this.fechaCompra = fechaCompra;
+    }
+
+    public int getIdFuncion() {
+        return idFuncion;
+    }
+
+    public void setIdFuncion(int idFuncion) {
+        this.idFuncion = idFuncion;
+    }
+}
