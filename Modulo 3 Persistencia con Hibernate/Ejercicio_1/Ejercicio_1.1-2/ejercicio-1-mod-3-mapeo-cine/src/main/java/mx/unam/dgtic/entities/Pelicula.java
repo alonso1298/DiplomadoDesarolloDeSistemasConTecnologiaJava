bbe2056c3d528a41d610d37964cb8979cabe57ac
alonso1298 <alonsosagrerogranados@gmail.com>
2025-11-15 11:14:40 -0600

    Add class pelicula

---
+++ Modulo 3 Persistencia con Hibernate/Ejercicio_1/Ejercicio_1.1-2/ejercicio-1-mod-3-mapeo-cine/src/main/java/mx/unam/dgtic/entities/Pelicula.java
@@ -0,0 +1,42 @@
+package mx.unam.dgtic.entities;
+
+public class Pelicula {
+    private int idPelicula;
+    private String titulo;
+    private int duracion;
+    private String clasificacion;
+
+    public Pelicula(){}
+
+    public int getIdPelicula() {
+        return idPelicula;
+    }
+
+    public void setIdPelicula(int idPelicula) {
+        this.idPelicula = idPelicula;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getClasificacion() {
+        return clasificacion;
+    }
+
+    public void setClasificacion(String clasificacion) {
+        this.clasificacion = clasificacion;
+    }
+}
