36ee7caa97da3f5a56b6b3aad3bcf7cc2c561635
alonso1298 <alonsosagrerogranados@gmail.com>
2025-11-15 11:13:15 -0600

    Add class funcion

---
+++ Modulo 3 Persistencia con Hibernate/Ejercicio_1/Ejercicio_1.1-2/ejercicio-1-mod-3-mapeo-cine/src/main/java/mx/unam/dgtic/entities/Funcion.java
@@ -0,0 +1,53 @@
+package mx.unam.dgtic.entities;
+
+import java.util.Date;
+
+public class Funcion {
+    private int idFuncion;
+    private Date fecha;
+    private Date hora;
+    private int idPelicula;
+    private int idSala;
+
+    public Funcion(){}
+
+    public int getIdFuncion() {
+        return idFuncion;
+    }
+
+    public void setIdFuncion(int idFuncion) {
+        this.idFuncion = idFuncion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Date getHora() {
+        return hora;
+    }
+
+    public void setHora(Date hora) {
+        this.hora = hora;
+    }
+
+    public int getIdPelicula() {
+        return idPelicula;
+    }
+
+    public void setIdPelicula(int idPelicula) {
+        this.idPelicula = idPelicula;
+    }
+
+    public int getIdSala() {
+        return idSala;
+    }
+
+    public void setIdSala(int idSala) {
+        this.idSala = idSala;
+    }
+}
