9a04cf89ad3571035f66de62d56b04524d05651a
alonso1298 <alonsosagrerogranados@gmail.com>
2025-11-20 20:40:02 -0600

    add class chapter

---
+++ Modulo 3 Persistencia con Hibernate/mi-proyecto-jpa/src/main/java/mx/unam/dgtic/entities/Chapter.java
@@ -0,0 +1,39 @@
+package mx.unam.dgtic.entities;
+
+public class Chapter {
+    private String bookIBN;
+    private int chapterNum;
+    private String title;
+
+    public Chapter(){}
+
+    public Chapter(String bookIBN, int chapterNum, String title) {
+        this.bookIBN = bookIBN;
+        this.chapterNum = chapterNum;
+        this.title = title;
+    }
+
+    public String getBookIBN() {
+        return bookIBN;
+    }
+
+    public void setBookIBN(String bookIBN) {
+        this.bookIBN = bookIBN;
+    }
+
+    public int getChapterNum() {
+        return chapterNum;
+    }
+
+    public void setChapterNum(int chapterNum) {
+        this.chapterNum = chapterNum;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
