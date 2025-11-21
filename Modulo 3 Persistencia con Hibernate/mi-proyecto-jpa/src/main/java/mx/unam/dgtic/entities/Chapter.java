d41efa3703052796ff0541cf60f2b799623b9f67
alonso1298 <alonsosagrerogranados@gmail.com>
2025-11-20 20:50:32 -0600

    add class anotation

--- Modulo 3 Persistencia con Hibernate/mi-proyecto-jpa/src/main/java/mx/unam/dgtic/entities/Chapter.java
+++ Modulo 3 Persistencia con Hibernate/mi-proyecto-jpa/src/main/java/mx/unam/dgtic/entities/Chapter.java
@@ -1,13 +1,18 @@
 package mx.unam.dgtic.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-
+@Entity
+@Table(name = "CHAPTER")
 public class Chapter {
     @Id
     private String bookIBN;
-
+    @Column(name = "BOOK_ISBN")
     private int chapterNum;
+    @Column(name = "TITLE")
     private String title;
 
     public Chapter(){}
