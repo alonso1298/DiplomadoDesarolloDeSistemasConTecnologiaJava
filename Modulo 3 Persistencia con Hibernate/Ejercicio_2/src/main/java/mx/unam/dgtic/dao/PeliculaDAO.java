ac3bcb2192a622f49506c9bfe9542b2c29735d00
alonso1298 <alonsosagrerogranados@gmail.com>
2025-11-21 18:07:13 -0600

    Add interface

--- Modulo 3 Persistencia con Hibernate/Ejercicio_2/src/main/java/mx/unam/dgtic/dao/PeliculaDAO.java
+++ Modulo 3 Persistencia con Hibernate/Ejercicio_2/src/main/java/mx/unam/dgtic/dao/PeliculaDAO.java
@@ -1,4 +1,32 @@
 package mx.unam.dgtic.dao;
 
-public class PeliculaDAO {
+import mx.unam.dgtic.entities.Pelicula;
+
+import java.util.List;
+
+public class PeliculaDAO implements ICineCRUD<Pelicula> {
+    @Override
+    public List<Pelicula> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Pelicula findById(String id) {
+        return null;
+    }
+
+    @Override
+    public void save(Pelicula pelicula) {
+
+    }
+
+    @Override
+    public void update(Pelicula pelicula) {
+
+    }
+
+    @Override
+    public void delete(String id) {
+
+    }
 }
