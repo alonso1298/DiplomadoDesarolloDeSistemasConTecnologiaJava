4185284fbf13e1acc050bd210ccfaca419e6fac7
alonso1298 <alonsosagrerogranados@gmail.com>
2025-11-21 20:38:20 -0600

    Refactor BookDAO

--- Modulo 3 Persistencia con Hibernate/jdbc-ejemplo/src/main/java/com/dgtic/unam/dao/BookDAO.java
+++ Modulo 3 Persistencia con Hibernate/jdbc-ejemplo/src/main/java/com/dgtic/unam/dao/BookDAO.java
@@ -1,32 +1,71 @@
 package com.dgtic.unam.dao;
 
 import com.dgtic.unam.model.Book;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 import java.util.List;
 
 public class BookDAO {
-    // SEELECT * WHERE
-    public Book findById(String id){
-        throw new UnsupportedOperationException("Not Soported yet.");
+
+    private SessionFactory sessionFactory;
+
+    public BookDAO() {
+        sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Book.class)
+                .buildSessionFactory();
     }
 
-    // SELECT *
+    // SELECT * FROM Books WHERE id = :id
+    public Book findById(String id) {
+        Session session = sessionFactory.openSession();
+
+        Book book = session.get(Book.class, id);
+
+        session.close();
+        return book;
+    }
+
     public List<Book> findAll(){
-        throw new UnsupportedOperationException("Not Soported yet.");
+        Session session = sessionFactory.openSession();
+
+        List<Book> books = session
+                .createQuery("FROM Book", Book.class)
+                .list();
+
+        session.close();
+        return books;
     }
 
-    // INSERT
-    public void save(){
-        throw new UnsupportedOperationException("Not Soported yet.");
+    public void save(Book book){
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        session.save(book);
+
+        session.getTransaction().commit();
+        session.close();
     }
 
-    // UPDATE
     public void update(Book book){
-        throw new UnsupportedOperationException("Not Soported yet.");
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        session.update(book);
+
+        session.getTransaction().commit();
+        session.close();
     }
 
-    // DELETE FROM
     public void delete(Book book){
-        throw new UnsupportedOperationException("Not Soported yet.");
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        session.delete(book);
+
+        session.getTransaction().commit();
+        session.close();
     }
 }
