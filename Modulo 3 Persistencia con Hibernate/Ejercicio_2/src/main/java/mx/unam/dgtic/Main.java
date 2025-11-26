cb278b40ae22088eff83da61bfbc5a57695abf8e
alonso1298 <alonsosagrerogranados@gmail.com>
2025-11-25 21:39:15 -0600

    add main

--- Modulo 3 Persistencia con Hibernate/Ejercicio_2/src/main/java/mx/unam/dgtic/Main.java
+++ Modulo 3 Persistencia con Hibernate/Ejercicio_2/src/main/java/mx/unam/dgtic/Main.java
@@ -1,15 +1,32 @@
 package mx.unam.dgtic;
 
-import mx.unam.dgtic.dao.PeliculaDAO;
 import mx.unam.dgtic.entities.Pelicula;
-
-import java.util.List;
+import mx.unam.dgtic.service.PeliculaService;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        PeliculaDAO peliculaDAO = new PeliculaDAO();
+        PeliculaService service = new PeliculaService();
 
+        // Insert
+        Pelicula nueva = new Pelicula();
+        nueva.setTitulo("Batman 2025");
+        nueva.setDuracion(150);
+        nueva.setClasificacion("PG-13");
+        service.crearPelicula(nueva);
+
+        // Select
+        service.obtenerPeliculas().forEach(
+                p -> System.out.println(p.getTitulo())
+        );
+
+        // Update
+        Pelicula p = service.obtenerPelicula(1);
+        p.setTitulo("Avengers: Endgame (Extended)");
+        service.actualizarPelicula(p);
+
+        // Delete
+        service.eliminarPelicula(3);
     }
 }
