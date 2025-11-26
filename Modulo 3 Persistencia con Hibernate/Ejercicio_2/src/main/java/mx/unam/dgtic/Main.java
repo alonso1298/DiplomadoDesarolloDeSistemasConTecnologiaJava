3d036c768e3427e8e93b102e141b259c564799e4
alonso1298 <alonsosagrerogranados@gmail.com>
2025-11-25 21:30:38 -0600

    delete class FuncionDAO

--- Modulo 3 Persistencia con Hibernate/Ejercicio_2/src/main/java/mx/unam/dgtic/Main.java
+++ Modulo 3 Persistencia con Hibernate/Ejercicio_2/src/main/java/mx/unam/dgtic/Main.java
@@ -1,6 +1,5 @@
 package mx.unam.dgtic;
 
-import mx.unam.dgtic.dao.FuncionDAO;
 import mx.unam.dgtic.dao.PeliculaDAO;
 import mx.unam.dgtic.entities.Pelicula;
 
@@ -11,21 +10,6 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         PeliculaDAO peliculaDAO = new PeliculaDAO();
-        FuncionDAO funcionDAO = new FuncionDAO();
-
-        Pelicula pelicula = peliculaDAO.findById(1);
-        //Funcion funcion = funcionDAO.findById(1);
-
-        // Metodos Extra con JPQL
-        List<Pelicula> buscarPeliculaDuracion = peliculaDAO.findByDuracionMayor(100);
-        List<Pelicula> bucarPeliculaNombre = peliculaDAO.findByName("coco");
-
-        System.out.println(pelicula.toString());
-        //System.out.println(funcion.toString());
-
-        // Metodo extra
-        System.out.println(buscarPeliculaDuracion);
-        System.out.println(bucarPeliculaNombre);
 
     }
 }
