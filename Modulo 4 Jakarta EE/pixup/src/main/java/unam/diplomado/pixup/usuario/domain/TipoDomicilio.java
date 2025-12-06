792f3756a6156327226cd0b3d9fedb255dc8ea97
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-12-06 10:03:13 -0600

    Add class with lombok

---
+++ Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/usuario/domain/TipoDomicilio.java
@@ -0,0 +1,13 @@
+package unam.diplomado.pixup.usuario.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class TipoDomicilio {
+    private Integer id;
+    private String descripcion;
+
+
+}
