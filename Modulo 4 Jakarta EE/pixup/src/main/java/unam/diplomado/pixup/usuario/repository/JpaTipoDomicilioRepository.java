bdc0ebc0aafaae771d0ef5881373da90a272c0fa
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-12-06 13:35:51 -0600

    Add implTipoDomicilio

---
+++ Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/usuario/repository/JpaTipoDomicilioRepository.java
@@ -0,0 +1,32 @@
+package unam.diplomado.pixup.usuario.repository;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import unam.diplomado.pixup.usuario.domain.TipoDomicilio;
+
+import java.util.Collection;
+import java.util.List;
+
+@ApplicationScoped // Todas se hacen es una sola instancia (singleton)
+public class JpaTipoDomicilioRepository implements ITipoDomicilioRepository{
+
+    @PersistenceContext(unitName = "pixup")
+    private EntityManager entityManager; // Se pueden hacer todas las persistencias con jpa
+
+    @Override
+    public Collection<TipoDomicilio> findAll() {
+        TypedQuery<TipoDomicilio> query =
+                entityManager.createQuery(
+                        "SELECT td FROM TipoDomicilio td", TipoDomicilio.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public TipoDomicilio findById(Integer id) {
+        TipoDomicilio tipoDomicilio = 
+                entityManager.find(TipoDomicilio.class, id);
+        return tipoDomicilio;
+    }
+}
