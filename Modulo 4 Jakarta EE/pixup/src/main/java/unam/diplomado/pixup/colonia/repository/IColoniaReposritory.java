3d8f3c57fd63009aae0ec93f88097666ef01fc26
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-12-06 14:01:34 -0600

    Implements persistence Colonia

---
+++ Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/colonia/repository/IColoniaReposritory.java
@@ -0,0 +1,12 @@
+package unam.diplomado.pixup.colonia.repository;
+
+import unam.diplomado.pixup.colonia.domain.Colonia;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IColoniaReposritory {
+
+    List<Colonia> finByCp(String cp);
+    Optional<Colonia> findById(Integer id);
+}
---
+++ Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/colonia/repository/JpaColoniaRepository.java
@@ -0,0 +1,31 @@
+package unam.diplomado.pixup.colonia.repository;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import unam.diplomado.pixup.colonia.domain.Colonia;
+
+import java.util.List;
+import java.util.Optional;
+
+@ApplicationScoped // Crea una unica isntancia del repositorio
+public class JpaColoniaRepository implements IColoniaReposritory{
+
+    @PersistenceContext(unitName = "pixup")
+    private EntityManager entityManager;
+
+    @Override
+    public List<Colonia> finByCp(String cp) {
+        TypedQuery<Colonia> query = entityManager.createQuery(
+                "SELECT c FROM Colonia c WHERE c.cp = ?1", Colonia.class);
+        query.setParameter(1, cp);
+        return query.getResultList();
+    }
+
+    @Override
+    public Optional<Colonia> findById(Integer id) {
+        Colonia colonia = entityManager.find(Colonia.class, id);
+        return colonia != null ? Optional.of(colonia) : Optional.empty();
+    }
+}
