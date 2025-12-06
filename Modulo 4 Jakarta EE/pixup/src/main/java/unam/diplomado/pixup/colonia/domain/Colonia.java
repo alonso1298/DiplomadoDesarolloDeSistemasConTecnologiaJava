bc5eeb4a58fd8e463485a4926233d4d0ba12370a
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-12-06 11:09:15 -0600

    Add entitie Colonia

--- Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/colonia/domain/Colonia.java
+++ Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/colonia/domain/Colonia.java
@@ -1,16 +1,23 @@
 package unam.diplomado.pixup.colonia.domain;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @Entity
-public class Colonia {
+public class Colonia implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nombre;
     private String codigoPostal;
+    @ManyToOne(targetEntity = Municipio.class)
+    @JoinColumn(name = "id_municipio", nullable = false)
+    private Municipio municipio;
 
 }
