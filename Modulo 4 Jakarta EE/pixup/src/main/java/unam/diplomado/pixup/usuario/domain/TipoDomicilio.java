55608d86976306865722273926cffc0ca3acbfe9
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-12-06 10:41:54 -0600

    Add entities

--- Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/usuario/domain/TipoDomicilio.java
+++ Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/usuario/domain/TipoDomicilio.java
@@ -4,11 +4,13 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
 @Entity
 @Table(name = "tipo_domicilio")
-public class TipoDomicilio {
+public class TipoDomicilio implements Serializable { // Transfoma la entidad a un conjunto de bytes para que se pueda transmitir en red
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
