9f8087d07315312da546f79564fa3033e0fa8202
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-12-06 10:20:59 -0600

    Add anotations jakarta and persistence

--- Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/usuario/domain/TipoDomicilio.java
+++ Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/usuario/domain/TipoDomicilio.java
@@ -1,11 +1,17 @@
 package unam.diplomado.pixup.usuario.domain;
 
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@Entity
+@Table(name = "tipo_domicilio")
 public class TipoDomicilio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String descripcion;
 
