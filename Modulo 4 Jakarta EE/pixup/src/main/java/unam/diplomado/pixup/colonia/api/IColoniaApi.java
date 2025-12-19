c91d41de65d082fe948744e60206ed5dd082f032
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-12-18 19:45:45 -0600

    Add interface

--- Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/colonia/api/IColoniaApi.java
+++ Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/colonia/api/IColoniaApi.java
@@ -1,4 +1,23 @@
 package unam.diplomado.pixup.colonia.api;
 
-public class IColoniaApi {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import unam.diplomado.pixup.colonia.domain.Colonia;
+
+import java.util.Collection;
+
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@Path("colonias")
+public interface IColoniaApi {
+
+    @GET
+    @Path("{id}")
+    Colonia getColoniaById(@PathParam("id") Integer id); //@PathParam("id") liga el valor para hacer la busqueda correspondiente
+    
+    @GET
+    Collection<Colonia> getColoniasByCp(@NotBlank @QueryParam("cp") String cp); // @QueryParam("cp") hace el ligado del valor
+      // @NotBlank por medio de la anotacion se puede aplicar validaciones en este caso que el valor no venga en blanco
+
 }
