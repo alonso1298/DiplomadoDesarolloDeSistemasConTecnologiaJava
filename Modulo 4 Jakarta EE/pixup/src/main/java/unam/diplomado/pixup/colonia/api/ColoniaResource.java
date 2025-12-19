6003256a7881ccb90f7bd32461cb554d03b2ee12
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-12-18 20:23:01 -0600

    add resource

--- Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/colonia/api/ColoniaResource.java
+++ Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/colonia/api/ColoniaResource.java
@@ -1,4 +1,33 @@
 package unam.diplomado.pixup.colonia.api;
 
-public class ColoniaResource {
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import unam.diplomado.pixup.colonia.domain.Colonia;
+import unam.diplomado.pixup.colonia.repository.IColoniaReposritory;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@RequestScoped // Por cada peticion se creara un nuevo objeto
+public class ColoniaResource implements IColoniaApi{
+
+    @Inject
+    private IColoniaReposritory coloniaReposritory;
+
+    @Override
+    public Colonia getColoniaById(Integer id) {
+        Optional<Colonia> colonia = coloniaReposritory.findById(id);
+        return colonia.orElse(null);
+        /*
+        if (colonia.isPresent()) {
+            return colonia.get();
+        }
+        return null;
+         */
+    }
+
+    @Override
+    public Collection<Colonia> getColoniasByCp(String cp) {
+        return coloniaReposritory.finByCp(cp);
+    }
 }
