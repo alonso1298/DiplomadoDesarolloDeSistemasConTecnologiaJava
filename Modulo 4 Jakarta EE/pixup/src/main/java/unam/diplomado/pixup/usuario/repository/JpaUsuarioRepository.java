ddad531f5a6fdc3151af84df44f6fe3642727ffa
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-01-10 13:27:23 -0600

    add implements Usuario

---
+++ Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/usuario/repository/JpaUsuarioRepository.java
@@ -0,0 +1,33 @@
+package unam.diplomado.pixup.usuario.repository;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import unam.diplomado.pixup.usuario.domain.Usuario;
+
+import java.util.List;
+import java.util.Optional;
+
+@ApplicationScoped
+public class JpaUsuarioRepository implements IUsuarioRepository {
+
+    @PersistenceContext(unitName = "pixup")
+    private EntityManager entityManager;
+
+    @Override
+    public Usuario save(Usuario usuario) {
+        entityManager.persist(usuario);
+        return usuario;
+    }
+
+    @Override
+    public Optional<Usuario> findByEmail(String email) {
+        TypedQuery<Usuario> query = entityManager.createQuery(
+                "SELECT u FROM Usuario u WHERE u.email = :email", Usuario.class
+        );
+        query.setParameter("email", email);
+        List<Usuario> usuarios = query.getResultList();
+        return !usuarios.isEmpty() ? Optional.of(usuarios.get(0)) : Optional.empty();
+    }
+}
