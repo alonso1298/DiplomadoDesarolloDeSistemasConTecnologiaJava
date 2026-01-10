6ce4d758f0d95d9924b6c00f5424604bc6c7b4e6
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-01-10 13:17:22 -0600

    add interface

---
+++ Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/usuario/repository/IDomicilioRepository.java
@@ -0,0 +1,9 @@
+package unam.diplomado.pixup.usuario.repository;
+
+import unam.diplomado.pixup.usuario.domain.Domicilio;
+
+public interface IDomicilioRepository {
+
+    Domicilio save(Domicilio domicilio);
+
+}
