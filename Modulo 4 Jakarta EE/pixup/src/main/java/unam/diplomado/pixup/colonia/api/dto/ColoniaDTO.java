63c6fe1dbe4e06bcbc10fb545d6bb5c690516541
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-01-10 12:07:10 -0600

    add dtoColonia

--- Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/colonia/api/dto/ColoniaDTO.java
+++ Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/colonia/api/dto/ColoniaDTO.java
@@ -1,9 +1,18 @@
 package unam.diplomado.pixup.colonia.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ColoniaDTO {
 
     private Integer id;
     private String nombre;
     private String cp;
+    private String minicipio;
+    private String estado;
 
 }
