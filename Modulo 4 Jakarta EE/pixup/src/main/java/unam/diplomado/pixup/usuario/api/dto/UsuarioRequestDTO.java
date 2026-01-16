9307fbb7b35b421bcb4c65cb58988d6568677373
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-01-16 17:55:53 -0600

    add UsuarioRequesDTO

--- Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/usuario/api/dto/UsuarioRequestDTO.java
+++ Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/usuario/api/dto/UsuarioRequestDTO.java
@@ -5,9 +5,7 @@ import jakarta.persistence.Column;
 public class UsuarioRequestDTO {
 
     private String nombre;
-    @Column(name = "primer_apellido")
     private String primerApellido;
-    @Column(name = "segundo_apellido")
     private String segundoApellido;
     private String password;
     private String email;
