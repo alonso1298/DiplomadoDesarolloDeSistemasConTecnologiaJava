b36fdcf6c35d46442b5a6d68e8a2cfdb7e6c7086
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-01-16 19:05:56 -0600

    refactor UsuarioApi

--- Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/usuario/api/IUsuarioApi.java
+++ Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/usuario/api/IUsuarioApi.java
@@ -1,11 +1,14 @@
 package unam.diplomado.pixup.usuario.api;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import unam.diplomado.pixup.usuario.api.dto.RegistroUsuarioDTO;
 
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
@@ -13,6 +16,7 @@ import jakarta.ws.rs.core.Response;
 public interface IUsuarioApi {
 
     @POST
-    Response registrarUsuario();
+    @Path("registro")
+    Response registrarUsuario(@NotNull @Valid RegistroUsuarioDTO registroUsuarioDTO);
 
 }
