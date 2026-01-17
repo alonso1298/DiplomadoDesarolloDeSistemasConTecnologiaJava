51e0e039bf2b91e9a19d9acd79e6469241d0488f
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-01-17 13:28:48 -0600

    add funcionality

--- Modulo 4 Jakarta EE/notificaciones/src/main/java/unam/diplomado/notificaciones/service/EmailServiceImpl.java
+++ Modulo 4 Jakarta EE/notificaciones/src/main/java/unam/diplomado/notificaciones/service/EmailServiceImpl.java
@@ -2,6 +2,11 @@ package unam.diplomado.notificaciones.service;
 
 import jakarta.annotation.Resource;
 import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
+import java.util.Date;
 
 public class EmailServiceImpl implements IEmailSerivce{
 
@@ -11,9 +16,18 @@ public class EmailServiceImpl implements IEmailSerivce{
     @Override
     public void enviarEmail(String email, String titulo, String mensaje) {
         try {
-
+            // Crear Email
+            MimeMessage message = new  MimeMessage(mailSession);
+            message.setRecipients(MimeMessage.RecipientType.TO,
+                    InternetAddress.parse(email, false));
+            message.setSubject(titulo);
+            message.setContent(mensaje, "text/html");
+            message.setSentDate(new Date());
+            // Enviar email
+            Transport.send(message);
         }catch (Exception e){
             e.printStackTrace();
+            throw new ServiceUnavailableException("EmailService");
 
         }
     }
