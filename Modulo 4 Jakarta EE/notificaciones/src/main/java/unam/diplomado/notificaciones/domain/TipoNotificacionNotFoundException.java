d621f07464a1ed79b68031472a857f616d00d63e
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-01-17 12:52:22 -0600

    add exception

---
+++ Modulo 4 Jakarta EE/notificaciones/src/main/java/unam/diplomado/notificaciones/domain/TipoNotificacionNotFoundException.java
@@ -0,0 +1,12 @@
+package unam.diplomado.notificaciones.domain;
+
+import jakarta.ejb.ApplicationException;
+
+@ApplicationException
+public class TipoNotificacionNotFoundException extends RuntimeException {
+
+    public TipoNotificacionNotFoundException(String descripcion) {
+        super("No se encontró el tipo de notificacion para el evento: " + descripcion);
+    }
+
+}
