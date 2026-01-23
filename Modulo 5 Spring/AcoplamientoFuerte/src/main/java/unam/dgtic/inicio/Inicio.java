956a320a94ca432fd6d822bc1c3b42522ba68296
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-01-23 17:06:40 -0600

    finish ejercicio1

--- Modulo 5 Spring/AcoplamientoFuerte/src/main/java/unam/dgtic/inicio/Inicio.java
+++ Modulo 5 Spring/AcoplamientoFuerte/src/main/java/unam/dgtic/inicio/Inicio.java
@@ -1,9 +1,13 @@
 package unam.dgtic.inicio;
 
 import unam.dgtic.modelo.Profesor;
+import unam.dgtic.modelo.Reponsabilidades;
 
 public class Inicio {
     public static void main(String[] args) {
         Profesor profesor = new Profesor();
+        Reponsabilidades reponsabilidades = new Reponsabilidades();
+        profesor.setResponsabilidades(reponsabilidades);
+        profesor.getResponsabilidades().calificar();
     }
 }
