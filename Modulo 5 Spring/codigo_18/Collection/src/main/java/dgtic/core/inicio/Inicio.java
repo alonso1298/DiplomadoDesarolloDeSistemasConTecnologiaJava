6795f78b39e05cc0b899e1150341c77f1e202845
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-01-24 11:08:06 -0600

    add set beans properties

--- Modulo 5 Spring/codigo_18/Collection/src/main/java/dgtic/core/inicio/Inicio.java
+++ Modulo 5 Spring/codigo_18/Collection/src/main/java/dgtic/core/inicio/Inicio.java
@@ -2,9 +2,12 @@ package dgtic.core.inicio;
 
 import dgtic.core.modelo.Empleado;
 import dgtic.core.modelo.EmpleadoM;
+import dgtic.core.modelo.EmpleadoP;
 import dgtic.core.modelo.IActividades;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Iterator;
 
 
 public class Inicio {
@@ -24,12 +27,23 @@ public class Inicio {
         for(IActividades actividades:empleadoDos.getActividades()) {
             actividades.realizar();
         }
+
         System.out.println("-----------------------------------------------");
         EmpleadoM empleadoTres = contexto.getBean("empleadoTres", EmpleadoM.class);
         System.out.println(empleadoTres);
         for(String llave:empleadoTres.getActividades().keySet()){
             System.out.println("Llave: " + llave);
             empleadoTres.getActividades().get(llave).realizar();
+        }
+
+        System.out.println("-----------------------------------------------");
+        EmpleadoP empleadoCuatro = contexto.getBean("empleadoCuatro", EmpleadoP.class);
+        System.out.println(empleadoCuatro);
+        for (Iterator<Object> iter = empleadoCuatro.getActividades().keySet().iterator();
+                iter.hasNext();){
+            String llave = (String) iter.next();
+            System.out.println("Llave: " + llave);
+            System.out.println(empleadoCuatro.getActividades().get(llave));
         }
 
         ((ClassPathXmlApplicationContext) contexto).close();
