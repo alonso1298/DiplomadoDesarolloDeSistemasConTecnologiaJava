4e0db20c9c3549e90262a0366eefee151ae225f4
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-01-23 19:25:47 -0600

    add class Persona

---
+++ Modulo 5 Spring/codigo_18/BeanFactory_2/src/main/java/unam/core/modelo/Persona.java
@@ -0,0 +1,24 @@
+package unam.core.modelo;
+
+public class Persona {
+
+    private String nombre;
+    private Integer edad;
+
+    public Persona() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public Integer getEdad() {
+        return edad;
+    }
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+}
