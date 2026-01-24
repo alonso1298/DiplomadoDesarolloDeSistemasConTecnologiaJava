46614cbf11252279eae13b5897fe44d375ab8cd5
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-01-24 10:26:26 -0600

    add project Collection

---
+++ Modulo 5 Spring/codigo_18/Collection/src/main/java/dgtic/core/inicio/Inicio.java
@@ -0,0 +1,35 @@
+package dgtic.core.inicio;
+
+import dgtic.core.modelo.Empleado;
+import dgtic.core.modelo.PlanificacionProyectos;
+import dgtic.core.modelo.Servicio;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class Inicio {
+    public static void main(String[] args) {
+        ApplicationContext contexto =
+                new ClassPathXmlApplicationContext(new String[] {
+                        "bean-configuration.xml"});
+        Empleado empleado = contexto.getBean("empleado", Empleado.class);
+        System.out.println(empleado);
+
+        /* PlanificacionProyectos planificacionProyectos = new PlanificacionProyectos();
+        empleado.setActividades(planificacionProyectos); */
+
+        empleado.getActividades().realizar();
+
+        System.out.println("..................");
+        Empleado empleadoDos = contexto.getBean("empleadoDos", Empleado.class);
+        System.out.println(empleadoDos);
+        empleadoDos.getActividades().realizar();
+
+        System.out.println("..................");
+        Empleado empleadoTres = contexto.getBean("empleadoTres", Empleado.class);
+        System.out.println(empleadoTres);
+        empleadoTres.getActividades().realizar();
+
+        ((ClassPathXmlApplicationContext) contexto).close();
+    }
+}
---
+++ Modulo 5 Spring/codigo_18/Collection/src/main/java/dgtic/core/modelo/Empleado.java
@@ -0,0 +1,50 @@
+package dgtic.core.modelo;
+
+public class Empleado {
+
+    private String nombre;
+    private int edad;
+    private IActividades actividades;
+
+    public Empleado(){}
+
+    public Empleado(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Empleado(IActividades actividades) {
+        this.actividades = actividades;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public IActividades getActividades() {
+        return actividades;
+    }
+
+    public void setActividades(IActividades actividades) {
+        this.actividades = actividades;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                '}';
+    }
+}
