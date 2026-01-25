a57ec0678f0b16973fd7ebeffada64afa15642d0
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-01-25 17:44:35 -0600

    add class

--- Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/disco/service/DiscoServiceImpl.java
+++ Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/disco/service/DiscoServiceImpl.java
@@ -1,13 +1,27 @@
 package unam.diplomado.pixup.disco.service;
 
 import jakarta.ejb.Stateless;
+import jakarta.inject.Inject;
 import unam.diplomado.pixup.disco.domain.Disco;
+import unam.diplomado.pixup.disco.repository.IArtistaRepository;
+import unam.diplomado.pixup.disco.repository.IDiscoRepository;
+import unam.diplomado.pixup.disco.repository.IDisqueraRepository;
+import unam.diplomado.pixup.disco.repository.IGeneroMusicalRepository;
 
 @Stateless
 public class DiscoServiceImpl implements IDiscoService{
-    
+
+    @Inject
+    private IDisqueraRepository disqueraRepository;
+    @Inject
+    private IArtistaRepository artistaRepository;
+    @Inject
+    private IGeneroMusicalRepository generoMusicalRepository;
+    @Inject
+    private IDiscoRepository discoRepository;
+
     @Override
     public Disco registrarDisco(Disco disco) {
-        return null;
+        return disco;
     }
 }
