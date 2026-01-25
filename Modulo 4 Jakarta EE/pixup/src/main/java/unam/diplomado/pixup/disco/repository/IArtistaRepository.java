ca63dcf261774ad89938a59bab556b3ff6390aaf
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-01-25 13:31:43 -0600

    add imports

--- Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/disco/repository/IArtistaRepository.java
+++ Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/disco/repository/IArtistaRepository.java
@@ -1,5 +1,7 @@
 package unam.diplomado.pixup.disco.repository;
 
+import unam.diplomado.pixup.disco.domain.Artista;
+
 import java.util.Optional;
 
 public interface IArtistaRepository {
--- Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/disco/repository/IGeneroMusicalRepository.java
+++ Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/disco/repository/IGeneroMusicalRepository.java
@@ -1,5 +1,7 @@
 package unam.diplomado.pixup.disco.repository;
 
+import unam.diplomado.pixup.disco.domain.GeneroMusical;
+
 import java.util.Optional;
 
 public interface IGeneroMusicalRepository {
