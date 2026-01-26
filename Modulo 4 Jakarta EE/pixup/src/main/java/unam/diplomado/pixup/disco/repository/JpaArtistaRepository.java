5f5ec00bf9f986e2a187713f354aa2a28842ef2b
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-01-25 21:22:32 -0600

    add Jpa

---
+++ Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/disco/repository/JpaArtistaRepository.java
@@ -0,0 +1,21 @@
+package unam.diplomado.pixup.disco.repository;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import unam.diplomado.pixup.disco.domain.Artista;
+
+import java.util.Optional;
+
+@ApplicationScoped
+public class JpaArtistaRepository implements IArtistaRepository{
+
+    @PersistenceContext(name = "pixup")
+    private EntityManager entityManager;
+
+    @Override
+    public Optional<Artista> finById(Integer id) {
+        Artista artista = entityManager.find(Artista.class, id);
+        return Optional.ofNullable(artista);
+    }
+}
