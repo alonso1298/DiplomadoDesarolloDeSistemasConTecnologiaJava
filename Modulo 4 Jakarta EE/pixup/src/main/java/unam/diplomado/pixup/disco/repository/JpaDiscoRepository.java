2509467ea7f2b2bc3d0c379492037140372e476e
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-01-25 20:24:54 -0600

    add implementation

--- Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/disco/repository/JpaDiscoRepository.java
+++ Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/disco/repository/JpaDiscoRepository.java
@@ -1,17 +1,33 @@
 package unam.diplomado.pixup.disco.repository;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import unam.diplomado.pixup.disco.domain.Disco;
 
 import java.util.Optional;
 
+@ApplicationScoped
 public class JpaDiscoRepository implements IDiscoRepository{
+
+    @PersistenceContext(name = "pixup")
+    private EntityManager entityManager;
+
     @Override
     public Disco save(Disco disco) {
-        return null;
+        entityManager.persist(disco);
+        return disco;
     }
 
     @Override
     public Optional<Disco> findByTituloAndArtista(String titulo, Integer idArtista) {
-        return Optional.empty();
+        TypedQuery<Disco> query = entityManager.createQuery(
+                "SELECT d FROM Disco d WHERE d.titulo = :titulo AND d.artista.id = :idArtista",
+                Disco.class
+        );
+        query.setParameter("titulo", titulo);
+        query.setParameter("idArtista", idArtista);
+        return query.getResultStream().findFirst();
     }
 }
