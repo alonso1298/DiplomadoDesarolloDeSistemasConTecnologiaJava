925fd50445fc0758104407abe2243b9e0d1de318
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-01-26 21:04:35 -0600

    add Response

--- Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/disco/api/DiscoResource.java
+++ Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/disco/api/DiscoResource.java
@@ -4,6 +4,8 @@ import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.core.Response;
 import unam.diplomado.pixup.disco.domain.Disco;
+import unam.diplomado.pixup.disco.domain.DiscoDuplicadoException;
+import unam.diplomado.pixup.disco.domain.DisqueraNotFoundException;
 import unam.diplomado.pixup.disco.service.IDiscoService;
 
 @RequestScoped
@@ -14,7 +16,30 @@ public class DiscoResource implements IDiscoApi {
 
     @Override
     public Response altaDisco(Disco disco) {
-        return null;
+        // Validar disco si ya existe
+        try {
+            Disco nuevo = discoService.registrarDisco(disco);
+            return Response.status(Response.Status.CREATED) // Codigo 201
+                    .entity(nuevo)
+                    .build();
+        }catch (DiscoDuplicadoException e){
+            return Response.status(Response.Status.CONFLICT) // Codigo 409
+                    .entity("El disco ya existe: " + e.getMessage())
+                    .build();
+
+        } catch (DisqueraNotFoundException e) { // Valida Disquera
+            return Response.status(422) // Unprocessable Content
+                    .entity("Disquera no encontrada: " + e.getMessage())
+                    .build();
+        }catch (IllegalArgumentException e){
+            return Response.status(Response.Status.BAD_REQUEST) // Codigo 400
+                    .entity("Datos invalidos: " + e.getMessage())
+                    .build();
+        }catch (Exception e) {
+            return Response.serverError()
+                    .entity("Error interno: " + e.getMessage())
+                    .build();
+        }
     }
 
 }
