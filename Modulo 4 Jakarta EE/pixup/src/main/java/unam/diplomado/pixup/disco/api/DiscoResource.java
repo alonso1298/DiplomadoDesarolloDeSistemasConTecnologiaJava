e5d7bbb2b44564dc9dc884042b39ab434a535b04
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-01-26 20:29:49 -0600

    add DiscoResource

---
+++ Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/disco/api/DiscoResource.java
@@ -0,0 +1,20 @@
+package unam.diplomado.pixup.disco.api;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.core.Response;
+import unam.diplomado.pixup.disco.domain.Disco;
+import unam.diplomado.pixup.disco.service.IDiscoService;
+
+@RequestScoped
+public class DiscoResource implements IDiscoApi {
+
+    @Inject
+    private IDiscoService discoService;
+
+    @Override
+    public Response altaDisco(Disco disco) {
+        return null;
+    }
+
+}
