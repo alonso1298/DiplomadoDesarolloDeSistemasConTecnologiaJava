bccbb2ad953d5603b342a9418338a964089553d9
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-01-26 21:27:27 -0600

    add anotation @Table in GeneroMusical

--- Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/disco/domain/GeneroMusical.java
+++ Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/disco/domain/GeneroMusical.java
@@ -1,15 +1,13 @@
 package unam.diplomado.pixup.disco.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @Entity
+@Table(name = "genero_musical")
 public class GeneroMusical {
 
     @Id
