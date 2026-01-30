d981e1f768e405da26eaf885fc34bcafeb372abc
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-01-30 17:58:26 -0600

    add pacjage

--- Modulo 5 Spring/codigo_18/JavaBean/src/main/java/dgtic/core/inicio/Inicio.java
+++ Modulo 5 Spring/codigo_18/JavaBean/src/main/java/dgtic/core/inicio/Inicio.java
@@ -1,16 +1,16 @@
 package dgtic.core.inicio;
 
-import dgtic.core.config.Configuracion;
 import dgtic.core.modelo.Persona;
 import dgtic.core.modelo.Telefono;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
+
 public class Inicio {
     public static void main(String[] args) {
         ConfigurableApplicationContext contexto
-                =new AnnotationConfigApplicationContext(Configuracion.class);
+                =new AnnotationConfigApplicationContext("dgtic.core");
         Persona persona=contexto.getBean("persona",Persona.class);
         persona.setEdad(100);
         System.out.println(persona);
@@ -18,8 +18,6 @@ public class Inicio {
         System.out.println(personaDos);
 
         Telefono telefono = contexto.getBean("telefono", Telefono.class);
-        System.out.println(telefono)
-        ;
         contexto.close();
     }
 }
