c838d9b517f7a33f84183bd5011a63c53a2a4e6c
alonso1298 <alonsosagrerogranados@gmail.com>
2026-01-31 13:09:58 -0600

    add test

--- Modulo 5 Spring/codigo_18/ventas-spring/src/test/java/dgtic_core/service/ProductoServicioTest.java
+++ Modulo 5 Spring/codigo_18/ventas-spring/src/test/java/dgtic_core/service/ProductoServicioTest.java
@@ -1,10 +1,23 @@
 package dgtic_core.service;
 
+import dgtic_core.model.Producto;
+import dgtic_core.repository.IProductoDao;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ProductoServicioTest {
 
+    @Mock
+    IProductoDao productoDao;
+
+    @InjectMocks
+    ProductoServicio productoServicio;
+
+    static ArrayList<Producto> listaProducto = new ArrayList<>();
 }
