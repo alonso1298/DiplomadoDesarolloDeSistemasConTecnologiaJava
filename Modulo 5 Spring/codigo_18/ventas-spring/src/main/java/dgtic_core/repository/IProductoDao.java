43e7ec00e36877511f7583dce9f6f59b4d5202e1
alonso1298 <alonsosagrerogranados@gmail.com>
2026-01-31 13:00:51 -0600

    add entity Persona

--- Modulo 5 Spring/codigo_18/ventas-spring/src/main/java/dgtic_core/repository/IProductoDao.java
+++ Modulo 5 Spring/codigo_18/ventas-spring/src/main/java/dgtic_core/repository/IProductoDao.java
@@ -1,4 +1,11 @@
 package dgtic_core.repository;
 
+import dgtic_core.model.Producto;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface IProductoDao {
+    List<Producto> lista() throws SecurityException;
+    void guardar(Producto producto) throws SQLException;
 }
--- Modulo 5 Spring/codigo_18/ventas-spring/src/main/java/dgtic_core/repository/ProductoDaoImpl.java
+++ Modulo 5 Spring/codigo_18/ventas-spring/src/main/java/dgtic_core/repository/ProductoDaoImpl.java
@@ -1,4 +1,20 @@
 package dgtic_core.repository;
 
-public class ProductoDaoImpl {
+import dgtic_core.model.Producto;
+import org.springframework.stereotype.Repository;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+public class ProductoDaoImpl implements IProductoDao{
+    @Override
+    public List<Producto> lista() throws SecurityException {
+        return List.of();
+    }
+
+    @Override
+    public void guardar(Producto producto) throws SQLException {
+
+    }
 }
