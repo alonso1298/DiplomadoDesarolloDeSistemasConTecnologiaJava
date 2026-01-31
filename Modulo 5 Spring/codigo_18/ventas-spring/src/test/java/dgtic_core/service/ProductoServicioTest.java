9141053bc1e8a6bbd3b950ee47c41f4251f33d93
alonso1298 <alonsosagrerogranados@gmail.com>
2026-01-31 13:16:43 -0600

    add mokito

--- Modulo 5 Spring/codigo_18/ventas-spring/src/test/java/dgtic_core/service/ProductoServicioTest.java
+++ Modulo 5 Spring/codigo_18/ventas-spring/src/test/java/dgtic_core/service/ProductoServicioTest.java
@@ -2,13 +2,19 @@ package dgtic_core.service;
 
 import dgtic_core.model.Producto;
 import dgtic_core.repository.IProductoDao;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class ProductoServicioTest {
@@ -20,4 +26,28 @@ class ProductoServicioTest {
     ProductoServicio productoServicio;
 
     static ArrayList<Producto> listaProducto = new ArrayList<>();
+
+    @BeforeAll // Se ejecuta una sola vez
+    static void estado(){
+        Producto producto = Producto.builder()
+                .id_producto(1)
+                .nombre("Producto 1")
+                .precio(234.89)
+                .stock(109)
+                .build();
+        listaProducto.add(producto);
+        producto = Producto.builder()
+                .id_producto(2)
+                .nombre("Producto 2")
+                .precio(234.09)
+                .stock(18)
+                .build();
+        listaProducto.add(producto);
+    }
+    @BeforeEach // Cada que se ejecute un test guarda y lo da
+    void inicio() throws SQLException{
+        when(productoDao.lista())
+                .thenReturn(listaProducto);
+        doNothing().when(productoDao).guardar(any(Producto.class));
+    }
 }
