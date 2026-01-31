e71e2a4e75d91cb5774ca8e034d1b15e9583a0ed
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-01-31 09:47:45 -0600

    add project

---
+++ Modulo 5 Spring/codigo_18/ventas-spring/src/test/java/dgtic_core/VentasSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dgtic_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentasSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
