d7d07f5057dbfeb1dce57f48d64d15dc1f1ebb60
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-01-30 18:16:18 -0600

    add project

---
+++ Modulo 5 Spring/codigo_18/PojoDao/src/main/java/dgtic/core/config/Configuracion.java
@@ -0,0 +1,18 @@
+package dgtic.core.config;
+
+import dgtic.core.modelo.Persona;
+import org.springframework.context.annotation.*;
+
+@Configuration
+@ImportResource(locations = {"classpath:bean-configuration.xml"})
+@ComponentScan(basePackages = "dgtic.core") // Se realiza un escano apartir de esa clase
+public class Configuracion {
+    @Bean(name = "persona")
+    @Scope("prototype")
+    public Persona getPersona(){
+        Persona persona=new Persona();
+        persona.setNombre("Persona 1");
+        persona.setEdad(23);
+        return persona;
+    }
+}
---
+++ Modulo 5 Spring/codigo_18/PojoDao/src/main/java/dgtic/core/inicio/Inicio.java
@@ -0,0 +1,29 @@
+package dgtic.core.inicio;
+
+import dgtic.core.config.Configuracion;
+import dgtic.core.modelo.Persona;
+import dgtic.core.modelo.Telefono;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+
+public class Inicio {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext contexto
+                =new AnnotationConfigApplicationContext(Configuracion.class); // se puede declarar tambine la ruta del proyecto "dgtic.core"
+        Persona persona=contexto.getBean("persona",Persona.class);
+        persona.setEdad(100);
+        System.out.println(persona);
+        Persona personaDos=contexto.getBean("persona",Persona.class);
+        System.out.println(personaDos);
+
+        Telefono telefono = contexto.getBean("telefono", Telefono.class);
+        System.out.println(telefono);
+
+        Persona persona1 = contexto.getBean("personaDos", Persona.class);
+        System.out.println(persona1);
+
+        contexto.close();
+    }
+}
