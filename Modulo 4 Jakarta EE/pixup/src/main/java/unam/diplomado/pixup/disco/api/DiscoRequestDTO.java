e4cd29bf4c3c76c65364b0067e1f2fd14ee9c149
alonso1298 <alonsosagrerogranados@gmail.com>
2026-01-31 21:48:58 -0600

    Se agrega request

---
+++ Modulo 4 Jakarta EE/pixup/src/main/java/unam/diplomado/pixup/disco/api/DiscoRequestDTO.java
@@ -0,0 +1,72 @@
+package unam.diplomado.pixup.disco.api;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import org.jetbrains.annotations.NotNull;
+
+public class DiscoRequestDTO {
+
+    @NotBlank
+    @Positive
+    private String titulo;
+
+    @NotNull
+    @Positive
+    private Integer artistaId;
+
+    @NotNull
+    @Positive
+    private Integer disqueraId;
+
+    @NotNull
+    @Positive
+    private Integer generoId;
+
+    @NotNull
+    @Positive
+    private Double precio;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    @NotNull
+    public Integer getArtistaId() {
+        return artistaId;
+    }
+
+    public void setArtistaId(@NotNull Integer artistaId) {
+        this.artistaId = artistaId;
+    }
+
+    @NotNull
+    public Integer getDisqueraId() {
+        return disqueraId;
+    }
+
+    public void setDisqueraId(@NotNull Integer disqueraId) {
+        this.disqueraId = disqueraId;
+    }
+
+    @NotNull
+    public Integer getGeneroId() {
+        return generoId;
+    }
+
+    public void setGeneroId(@NotNull Integer generoId) {
+        this.generoId = generoId;
+    }
+
+    @NotNull
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(@NotNull Double precio) {
+        this.precio = precio;
+    }
+}
