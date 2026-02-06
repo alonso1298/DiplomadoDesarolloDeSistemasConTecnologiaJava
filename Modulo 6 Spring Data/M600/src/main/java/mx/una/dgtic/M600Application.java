a62c52f446035fb648510e708434dd960b5ecbe9
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-02-06 17:01:25 -0600

    add configuration

--- Modulo 6 Spring Data/M600/src/main/java/mx/una/dgtic/M600Application.java
+++ Modulo 6 Spring Data/M600/src/main/java/mx/una/dgtic/M600Application.java
@@ -2,12 +2,45 @@ package mx.una.dgtic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @SpringBootApplication
 public class M600Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(M600Application.class, args);
+	}
+
+	@Bean
+	public DataSource dataSource(){
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/modulo06");
+		dataSource.setUsername("root");
+		dataSource.setPassword("56457977Ac");
+		return dataSource;
+	}
+
+	@Bean
+	LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(DataSource dataSource){
+		LocalContainerEntityManagerFactoryBean emfb =
+				new LocalContainerEntityManagerFactoryBean();
+		emfb.setDataSource(dataSource);
+		emfb.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+		emfb.setPackagesToScan("mx.una.dgtic");
+
+		Properties jpaProperties = new Properties();
+		jpaProperties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+
+		emfb.setJpaProperties(jpaProperties);
+
+		return emfb;
 	}
 
 }
