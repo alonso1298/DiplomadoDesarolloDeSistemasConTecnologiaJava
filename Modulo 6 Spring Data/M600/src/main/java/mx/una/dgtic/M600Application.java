512624be520ad8269e044a4c82fc72470a6b0dfb
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-02-06 16:42:15 -0600

    add project M6

---
+++ Modulo 6 Spring Data/M600/src/main/java/mx/una/dgtic/M600Application.java
@@ -0,0 +1,13 @@
+package mx.una.dgtic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M600Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M600Application.class, args);
+	}
+
+}
