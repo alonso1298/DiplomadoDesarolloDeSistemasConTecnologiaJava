a1e0e8dfb2e06bb6a22003504e3c92669fa41554
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-02-07 09:22:10 -0600

    add test

--- Modulo 6 Spring Data/M602/src/main/java/mx/unam/dgtic/repository/IAlumnoRepository.java
+++ Modulo 6 Spring Data/M602/src/main/java/mx/unam/dgtic/repository/IAlumnoRepository.java
@@ -3,5 +3,13 @@ package mx.unam.dgtic.repository;
 import mx.unam.dgtic.entity.Alumno;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface IAlumnoRepository extends CrudRepository<Alumno, String> {
+
+    // Consultas derivadas
+    List<Alumno> findByNombre(String nombre);
+
+
+
 }
