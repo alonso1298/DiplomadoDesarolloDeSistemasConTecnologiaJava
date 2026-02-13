172eed8d0360d35466c41b13716f6d8f1104e456
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-02-13 16:32:47 -0600

    add entity Alumno

---
+++ Modulo 6 Spring Data/M603/src/main/java/mx/unam/dgtic/entity/Alumno.java
@@ -0,0 +1,105 @@
+package mx.unam.dgtic.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "Alumnos")
+public class Alumno {
+    @Id
+    private String matricula;
+    private String nombre;
+    private String paterno;
+    private Date fnac;
+    private double estatura;
+    @ManyToOne
+    @JoinColumn(name = "estado_id_estado")
+    private Estado estado;
+    private String curp;
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    public Alumno() {
+    }
+
+    public Alumno(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public Alumno(String matricula, String nombre, String paterno, Date fnac, double estatura, String curp) {
+        this.matricula = matricula;
+        this.nombre = nombre;
+        this.paterno = paterno;
+        this.fnac = fnac;
+        this.estatura = estatura;
+        this.curp = curp;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPaterno() {
+        return paterno;
+    }
+
+    public void setPaterno(String paterno) {
+        this.paterno = paterno;
+    }
+
+    public Date getFnac() {
+        return fnac;
+    }
+
+    public void setFnac(Date fnac) {
+        this.fnac = fnac;
+    }
+
+    public double getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(double estatura) {
+        this.estatura = estatura;
+    }
+
+    public String getCurp() {
+        return curp;
+    }
+
+    public void setCurp(String curp) {
+        this.curp = curp;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" +
+                "matricula='" + matricula + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", paterno='" + paterno + '\'' +
+                ", fnac=" + fnac +
+                ", estatura=" + estatura +
+                ", estado=" + estado +
+                ", curp='" + curp + '\'' +
+                '}';
+    }
+}
