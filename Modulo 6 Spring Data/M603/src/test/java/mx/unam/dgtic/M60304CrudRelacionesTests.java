3c68457c10795dfe23459be7856adb64f4547f6e
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-02-14 09:24:06 -0600

    add complete test

--- Modulo 6 Spring Data/M603/src/test/java/mx/unam/dgtic/M60304CrudRelacionesTests.java
+++ Modulo 6 Spring Data/M603/src/test/java/mx/unam/dgtic/M60304CrudRelacionesTests.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,13 +18,14 @@ import java.util.Optional;
 
 @SpringBootTest
 @Transactional
-@Rollback(value = false)
+//@Rollback(value = false)
+@Commit
 public class M60304CrudRelacionesTests {
     final String ALUMNO = "ALONSO SAGRERO GRANADOS";
     private static final String ESTADO = "Guerrero";
     private static final String GRUPO = "Primero";
     private static final Integer CALIFICACION = 8;
-    private static final String MATRICULA = "1A";
+    private static final String MATRICULA = "1F";
     private static final String MATERIA = "BD";
 
     @Autowired
@@ -37,26 +39,31 @@ public class M60304CrudRelacionesTests {
         System.out.println(ALUMNO);
         System.out.println("Crear un alumno ");
 
-        Estado estado = repositorioEstado.findByEstado(ESTADO);
-        if (estado == null){
-            throw new RuntimeException("Estado no localizado");
+        Optional<Alumno> optional = repositorioAlumno.findById(MATRICULA);
+        if(optional.isEmpty()){
+
+            Estado estado = repositorioEstado.findByEstado(ESTADO);
+            if (estado == null){
+                throw new RuntimeException("Estado no localizado");
+            }
+
+            Alumno alumno = new Alumno(MATRICULA, "Alonso", "Sagrero",
+                    Date.valueOf("1998-12-12"), 1.74, "SAGA981212HDFGRL08");
+
+            alumno.setEstado(estado);
+            Perfil perfil = new Perfil();
+            perfil.setBiografia("Ingeniero en computacion");
+            perfil.setIntereses("Leer, Deporte, Cine");
+            perfil.setHabilidades("Poo");
+
+            perfil.setAlumno(alumno);
+            alumno.setPerfil(perfil);
+
+            repositorioAlumno.save(alumno);
         }
-        Alumno alumno = new Alumno("1F", "Alonso", "Sgrero",
-                Date.valueOf("1998-12-12"), 1.74, "SAGA981212HDFGRL08");
-
-        alumno.setEstado(estado);
-        Perfil perfil = new Perfil();
-        perfil.setBiografia("Ingeniero en computacion");
-        perfil.setIntereses("Leer, Deporte, Cine");
-        perfil.setHabilidades("Poo");
-
-        perfil.setAlumno(alumno);
-        alumno.setPerfil(perfil);
-
-        repositorioAlumno.save(alumno);
 
         System.out.println("Verificar persistencia de alumno");
-        Optional<Alumno> optional = repositorioAlumno.findById("1F");
+        optional = repositorioAlumno.findById(MATRICULA);
         if (optional.isPresent()){
             System.out.println("Alumno guardado exitosamente");
             System.out.println(optional.get());
