176ac55908b1380c5159b179620713d26c128208
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-02-21 10:43:02 -0600

    add endpoint

--- Modulo 7_Desarrollo de aplicaciones Web con Spring Web MVC/spring-web/spring-web/src/main/java/dgtic/core/controller/InicioController.java
+++ Modulo 7_Desarrollo de aplicaciones Web con Spring Web MVC/spring-web/spring-web/src/main/java/dgtic/core/controller/InicioController.java
@@ -1,6 +1,7 @@
 package dgtic.core.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -8,6 +9,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class InicioController {
     @RequestMapping(value = "/",method = RequestMethod.GET)
     public String inicio(){
+        return "inicio";
+    }
+    @RequestMapping(value = "modelo",method = RequestMethod.GET)
+    public String modelo(Model modelo){
+        modelo.addAttribute("mensaje","Diplomado de Java con Spring");
         return "inicio";
     }
 }
