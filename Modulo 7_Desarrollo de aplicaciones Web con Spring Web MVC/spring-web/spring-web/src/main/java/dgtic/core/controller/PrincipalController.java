4e22d5525af21d194762ec44e0188f25caca74d0
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-02-21 13:38:36 -0600

    add endpoint

--- Modulo 7_Desarrollo de aplicaciones Web con Spring Web MVC/spring-web/spring-web/src/main/java/dgtic/core/controller/PrincipalController.java
+++ Modulo 7_Desarrollo de aplicaciones Web con Spring Web MVC/spring-web/spring-web/src/main/java/dgtic/core/controller/PrincipalController.java
@@ -2,6 +2,7 @@ package dgtic.core.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,7 +11,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class PrincipalController {
     @RequestMapping(value = "requestmapping", method = RequestMethod.GET)
     public String mappingCLase(Model model){
-        model.addAttribute("contenido", "RequestMapping Clase");
-        return ""
+        model.addAttribute("contenido", "@RequestMapping Clase");
+        return "spring/requestmapping";
     }
+    @GetMapping("getmapping")
+    public String getMapping(Model model){
+        model.addAttribute("contenido", "@Getmapping en metodo");
+        return "spring/getmapping";
+    }
+
 }
