21f05088d1376a4e3605c490f53deacc30110218
alonso1298 <alonsosagrerogranados@gmail.com>
2026-02-24 20:04:50 -0600

    refactor

--- Modulo 7_Desarrollo de aplicaciones Web con Spring Web MVC/spring-web/spring-web/src/main/java/dgtic/core/controller/InicioController.java
+++ Modulo 7_Desarrollo de aplicaciones Web con Spring Web MVC/spring-web/spring-web/src/main/java/dgtic/core/controller/InicioController.java
@@ -6,8 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.swing.plaf.PanelUI;
-
 @Controller
 public class InicioController {
     @Value("${mensaje.application}")
