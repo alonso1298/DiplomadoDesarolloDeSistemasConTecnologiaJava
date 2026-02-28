7a5db9b654f444a08b5291ed119e3453fa8de952
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-02-28 10:44:40 -0600

    add validation

--- Modulo 7_Desarrollo de aplicaciones Web con Spring Web MVC/spring-web/spring-web/src/main/java/dgtic/core/validation/NombresValidation.java
+++ Modulo 7_Desarrollo de aplicaciones Web con Spring Web MVC/spring-web/spring-web/src/main/java/dgtic/core/validation/NombresValidation.java
@@ -1,9 +1,11 @@
 package dgtic.core.validation;
 
 import dgtic.core.model.dto.UsuarioDTO;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+@Component
 public class NombresValidation implements Validator {
     @Override
     public boolean supports(Class<?> clazz) {
