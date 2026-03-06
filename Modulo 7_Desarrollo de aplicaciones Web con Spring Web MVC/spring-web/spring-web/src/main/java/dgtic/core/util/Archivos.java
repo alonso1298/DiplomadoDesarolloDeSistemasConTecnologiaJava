594efdf629baa0e5fba64b497ae75ce104d2fb2d
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-03-06 17:14:02 -0600

    add archivo

--- Modulo 7_Desarrollo de aplicaciones Web con Spring Web MVC/spring-web/spring-web/src/main/java/dgtic/core/util/Archivos.java
+++ Modulo 7_Desarrollo de aplicaciones Web con Spring Web MVC/spring-web/spring-web/src/main/java/dgtic/core/util/Archivos.java
@@ -1,4 +1,28 @@
 package dgtic.core.util;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Archivos {
+    public static String almacenar(MultipartFile multipartFile, String direccion){
+        String nombre = multipartFile.getOriginalFilename();
+        Path path = Paths.get(direccion);
+        direccion=path.toFile().getAbsolutePath();
+
+        File imagen = new File(direccion+"/"+nombre);
+        Path finalRuta=Paths.get(imagen.getAbsolutePath());
+        try {
+            byte[] bytes=multipartFile.getBytes();
+            Files.write(finalRuta, bytes);
+            return nombre;
+        }catch (IOException ex){
+            ex.printStackTrace();
+            return null;
+        }
+    }
 }
