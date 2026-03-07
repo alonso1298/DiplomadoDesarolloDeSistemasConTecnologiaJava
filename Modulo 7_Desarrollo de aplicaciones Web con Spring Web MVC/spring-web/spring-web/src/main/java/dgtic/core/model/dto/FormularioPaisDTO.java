e6e067d8d249aa8fdffb8de24b9ec94cf2614d39
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-03-07 10:26:11 -0600

    add repository

---
+++ Modulo 7_Desarrollo de aplicaciones Web con Spring Web MVC/spring-web/spring-web/src/main/java/dgtic/core/model/dto/FormularioPaisDTO.java
@@ -0,0 +1,13 @@
+package dgtic.core.model.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FormularioPaisDTO {
+    private Long paisId;
+    private Long ciudadId;
+}
---
+++ Modulo 7_Desarrollo de aplicaciones Web con Spring Web MVC/spring-web/spring-web/src/main/java/dgtic/core/repository/IProductoRepository.java
@@ -0,0 +1,17 @@
+package dgtic.core.repository;
+
+import dgtic.core.model.dto.ProductoDTO;
+import dgtic.core.model.entity.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface IProductoRepository extends JpaRepository<Producto, Long> {
+    public interface ProductoRepository extends JpaRepository<Producto,Long> {
+        @Query("SELECT new dgtic.core.model.dto.ProductoDTO(p.producto_id,p.nombre) "+
+                " FROM Producto p WHERE p.categoria.categoria_id=:categoriaId")
+        List<ProductoDTO> findProductoByCategoria(@Param("categoriaId") Long categoriaId);
+    }
+}
