9b8f09be3ffe23731115d834db8efa897503089b
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-03-07 14:06:31 -0600

    refactor

--- Modulo 7_Desarrollo de aplicaciones Web con Spring Web MVC/ventas-estructura/src/main/java/dgtic/core/model/entity/Producto.java
+++ Modulo 7_Desarrollo de aplicaciones Web con Spring Web MVC/ventas-estructura/src/main/java/dgtic/core/model/entity/Producto.java
@@ -8,24 +8,24 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Entity
-@Table(name = "paroducto")
+@Table(name = "producto")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class Producto {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id_producto;
+    @Column(name = "id_producto")
+    private Long idProducto;
 
     private String nombre;
     private BigDecimal precio;
     private Integer stock;
     private String imagen;
 
-    @OneToMany(mappedBy = "producto",
-        orphanRemoval = true)
+    @OneToMany(mappedBy = "producto", orphanRemoval = true)
     private List<DetalleVenta> detalleVenta;
-
 }
