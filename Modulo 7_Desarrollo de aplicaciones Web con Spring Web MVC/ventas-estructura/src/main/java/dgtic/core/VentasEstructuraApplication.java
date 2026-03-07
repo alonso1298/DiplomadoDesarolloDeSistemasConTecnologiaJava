a7726bbfbaa41370f85353b01f6188387ffdf79c
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-03-07 13:08:16 -0600

    add project

---
+++ Modulo 7_Desarrollo de aplicaciones Web con Spring Web MVC/ventas-estructura/src/main/java/dgtic/core/VentasEstructuraApplication.java
@@ -0,0 +1,13 @@
+package dgtic.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentasEstructuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentasEstructuraApplication.class, args);
+	}
+
+}
