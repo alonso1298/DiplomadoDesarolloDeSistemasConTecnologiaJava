9c60435467c04e4d4132df2ded7d746ce882ff68
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-03-13 17:53:57 -0600

    add query

--- Modulo 7_Desarrollo de aplicaciones Web con Spring Web MVC/ventas-estructura/src/main/java/dgtic/core/model/dto/dto/cliente/ClienteBusquedaDTO.java
+++ Modulo 7_Desarrollo de aplicaciones Web con Spring Web MVC/ventas-estructura/src/main/java/dgtic/core/model/dto/dto/cliente/ClienteBusquedaDTO.java
@@ -1,5 +1,12 @@
 package dgtic.core.model.dto.dto.cliente;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ClienteBusquedaDTO {
     private Long id_cliente;
     private String nombre;
--- Modulo 7_Desarrollo de aplicaciones Web con Spring Web MVC/ventas-estructura/src/main/java/dgtic/core/repository/IClienteRepository.java
+++ Modulo 7_Desarrollo de aplicaciones Web con Spring Web MVC/ventas-estructura/src/main/java/dgtic/core/repository/IClienteRepository.java
@@ -1,13 +1,16 @@
 package dgtic.core.repository;
 
+import dgtic.core.model.dto.dto.cliente.ClienteBusquedaDTO;
 import dgtic.core.model.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IClienteRepository extends JpaRepository<Cliente, Long> {
+    
     @Query("""
             SELECT c
                    FROM Cliente c
@@ -15,7 +18,12 @@ public interface IClienteRepository extends JpaRepository<Cliente, Long> {
                    WHERE c.id_cliente = :id
             """)
     Optional<Cliente> findById(@Param("id") Long id);
+
     @Query("SELECT new dgtic.core.model.dto.cliente.ClienteBusquedaDTO(a.id_cliente,a.nombre)" +
             " FROM Cliente a WHERE a.nombre LIKE %?1% or a.email LIKE %?1%")
     List<ClienteBusquedaDTO> findClienteView(String dato);
+
+    @Query("SELECT DISTINCT a.ciudad" +
+            " FROM Cliente a")
+    List<String> findCiudadView();
 }
