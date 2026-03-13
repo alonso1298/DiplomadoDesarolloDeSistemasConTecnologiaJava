bd538ef8eff3c87f9488a417e9f5a55837ab48ed
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-03-13 17:03:39 -0600

    add index

--- Modulo 7_Desarrollo de aplicaciones Web con Spring Web MVC/ventas-estructura/src/main/java/dgtic/core/controller/InicioController.java
+++ Modulo 7_Desarrollo de aplicaciones Web con Spring Web MVC/ventas-estructura/src/main/java/dgtic/core/controller/InicioController.java
@@ -1,4 +1,14 @@
 package dgtic.core.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class InicioController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String inicio(Model model){
+        return "index";
+    }
 }
