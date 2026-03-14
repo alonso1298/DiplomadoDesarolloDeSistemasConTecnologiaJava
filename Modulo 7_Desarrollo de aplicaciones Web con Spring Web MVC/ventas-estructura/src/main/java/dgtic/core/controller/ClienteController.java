bde8c0069078c890431e86804f710802ce0167f4
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-03-13 18:26:39 -0600

    add utils

--- Modulo 7_Desarrollo de aplicaciones Web con Spring Web MVC/ventas-estructura/src/main/java/dgtic/core/controller/ClienteController.java
+++ Modulo 7_Desarrollo de aplicaciones Web con Spring Web MVC/ventas-estructura/src/main/java/dgtic/core/controller/ClienteController.java
@@ -1,9 +1,28 @@
 package dgtic.core.controller;
 
+import dgtic.core.service.cliente.IClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("clientes")
 public class ClienteController {
+    @Autowired
+    private IClienteService clienteService;
+
+    @GetMapping("lista-clientes")
+    public String listaProductos(
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            Model model) {
+        Pageable pageable = PageRequest.of(page, 5);
+        Page<ClienteDTO> clienteDTOS = clienteService.findByPage(pageable);
+        RenderPagina<ClienteDTO> renderPagina =
+                new RenderPagina<>("lista-clientes", clienteDTOS);
+        model.addAttribute("clientes", clienteDTOS);
+        model.addAttribute("page", renderPagina);
+        model.addAttribute("contenido", "Lista de Clientes");
+
+        return "clientes/lista-cliente";
+    }
 }
