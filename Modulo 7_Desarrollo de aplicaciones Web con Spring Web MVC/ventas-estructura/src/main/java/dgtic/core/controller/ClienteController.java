941e85336b1621f8072aa9b4a4d41375ec982cc6
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-03-13 19:21:48 -0600

    add endpoint

--- Modulo 7_Desarrollo de aplicaciones Web con Spring Web MVC/ventas-estructura/src/main/java/dgtic/core/controller/ClienteController.java
+++ Modulo 7_Desarrollo de aplicaciones Web con Spring Web MVC/ventas-estructura/src/main/java/dgtic/core/controller/ClienteController.java
@@ -4,6 +4,7 @@ import dgtic.core.model.dto.dto.cliente.ClienteDTO;
 import dgtic.core.service.cliente.IClienteService;
 import dgtic.core.util.RenderPagina;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -13,11 +14,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("clientes")
 public class ClienteController {
+
     @Autowired
     private IClienteService clienteService;
+
+    @Value("${ejemplo.imagen.ruta}")
+    private String archivoRuta;
 
     @GetMapping("lista-clientes")
     public String listaProductos(
@@ -32,5 +39,12 @@ public class ClienteController {
         model.addAttribute("contenido", "Lista de Clientes");
 
         return "clientes/lista-cliente";
+    }
+    @GetMapping("clientes-pdf")
+    public String generarPdf(Model model) {
+        List<ClienteDTO> clienteDTO = clienteService.findAll();
+        model.addAttribute("datos", clienteDTO);
+        model.addAttribute("ruta", archivoRuta);
+        return "clientes/clientes-pdf";
     }
 }
