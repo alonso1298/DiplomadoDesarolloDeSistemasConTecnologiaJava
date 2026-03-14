5d9575ec72ebba13d53f38208560416f4cf0b206
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-03-14 10:26:09 -0600

    add project

---
+++ Modulo 8 API REST/intro_m9_18/src/test/java/mx/unam/dgtic/IntroM918ApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.unam.dgtic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroM918ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
