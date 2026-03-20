1292eeb0d252810554638dacbb31d7d70578c533
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-03-20 16:47:19 -0600

    add project

---
+++ Modulo 8 API REST/alumnoapirest/src/test/java/mx/unam/dgtic/AlumnoapirestApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.unam.dgtic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumnoapirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
