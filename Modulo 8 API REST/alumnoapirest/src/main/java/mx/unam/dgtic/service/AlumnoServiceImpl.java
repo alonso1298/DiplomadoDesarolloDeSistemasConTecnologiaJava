f2a9fbfb26a62b99435f5123d4adf8c24197d6fa
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-03-20 19:21:13 -0600

    service alumno

--- Modulo 8 API REST/alumnoapirest/src/main/java/mx/unam/dgtic/service/AlumnoServiceImpl.java
+++ Modulo 8 API REST/alumnoapirest/src/main/java/mx/unam/dgtic/service/AlumnoServiceImpl.java
@@ -1,12 +1,14 @@
 package mx.unam.dgtic.service;
 
 import mx.unam.dgtic.entities.Alumno;
+import mx.unam.dgtic.entities.Calificacion;
 import mx.unam.dgtic.repositories.IAlumnoRepository;
 import mx.unam.dgtic.service.interfaces.IAlumnoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,22 +20,44 @@ public class AlumnoServiceImpl implements IAlumnoService {
 
     @Override
     public List<Alumno> obtenerTodos() {
-        return List.of();
+        return alumnoRepository.findAll();
     }
 
     @Override
     public Alumno obtenerAlumnoPorId(Long id) {
-        return null;
+        return alumnoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Aluno no encontrado"));
     }
 
     @Override
     public Alumno guardar(Alumno alumno) {
-        return null;
+        // trae calificaciones
+        if (alumno.getCalificaciones() != null){
+            for (Calificacion calificacion : alumno.getCalificaciones()) {
+                calificacion.setAlumno(alumno);
+            }
+        }
+        return alumnoRepository.save(alumno);
     }
 
     @Override
     public Alumno actualizarCompleto(Long id, Alumno alumno) {
-        return null;
+        Alumno alumnoDb = alumnoRepository.findById(id)
+                .orElseThrow(() -> new  RuntimeException("Alumno no encontrado con id: " + id));
+        // forzando a actualizar todos los campos
+        alumnoDb.setNombre(alumno.getNombre());
+        alumnoDb.setApellido(alumno.getApellido());
+        alumnoDb.setCorreo(alumno.getCorreo());
+
+        if (alumno.getCalificaciones() != null){
+            alumnoDb.setCalificaciones(new ArrayList<>(alumno.getCalificaciones()));
+            for (Calificacion calificacion : alumno.getCalificaciones()){
+                calificacion.setAlumno(alumnoDb);
+            }
+        }else {
+            alumnoDb.setCalificaciones(null);
+        }
+        return alumnoRepository.save(alumnoDb);
     }
 
     @Override
