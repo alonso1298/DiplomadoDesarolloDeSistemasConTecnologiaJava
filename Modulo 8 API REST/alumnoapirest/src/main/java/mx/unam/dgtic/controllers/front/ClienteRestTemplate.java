e0cc527f3a1ca7e02f6d2a997b36bd8ad807dba9
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-03-28 09:06:39 -0600

    add restTemplate

--- Modulo 8 API REST/alumnoapirest/src/main/java/mx/unam/dgtic/controllers/front/ClienteRestTemplate.java
+++ Modulo 8 API REST/alumnoapirest/src/main/java/mx/unam/dgtic/controllers/front/ClienteRestTemplate.java
@@ -4,6 +4,7 @@ import mx.unam.dgtic.dto.AlumnoDto;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
@@ -23,11 +24,12 @@ public class ClienteRestTemplate {
     public String index(){
         return "index";
     }
-    
+
     @GetMapping("/")
-    public String getTodos() {
+    public String getTodos(Model model) {
         List<AlumnoDto> alumnos = restTemplate.getForObject(url_base, List.class);
         LoggerFactory.getLogger(ClienteRestTemplate.class).info("Alumnos REST TEMPLATE" + alumnos.toString());
+        model.addAttribute("alumnos", alumnos);
         return "todos";
     }
 }
