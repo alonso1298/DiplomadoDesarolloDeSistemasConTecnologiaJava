855e0ea4ca2a252cfba94ea1ac2a9a266e82f79e
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-03-28 10:29:41 -0600

    add form

--- Modulo 8 API REST/alumnoapirest/src/main/java/mx/unam/dgtic/service/WebClientAlumnoDtoService.java
+++ Modulo 8 API REST/alumnoapirest/src/main/java/mx/unam/dgtic/service/WebClientAlumnoDtoService.java
@@ -4,6 +4,7 @@ import mx.unam.dgtic.dto.AlumnoDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -25,5 +26,21 @@ public class WebClientAlumnoDtoService {
                 .uri("/{id}", id)
                 .retrieve()
                 .bodyToMono(AlumnoDto.class);
+    }
+
+    public Mono<AlumnoDto> guardarAlumno(AlumnoDto alumnoDto){
+        return webClient.post()
+                .uri("/")
+                .bodyValue(alumnoDto)
+                .retrieve()
+                .bodyToMono(AlumnoDto.class);
+    }
+
+    //GET: /web/nuevo --> Vista -> HTML forma para dar el alta
+    // POST: /web/alumno/
+    @GetMapping("/web/nuevo/")
+    public String mostrarFormulario(Model model){
+        model.addAttribute("alumno", new AlumnoDto());
+        return "vista/formulario";
     }
 }
