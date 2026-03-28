1b12107c4217d17daa93d90afe1b05dd16fd70cc
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-03-27 18:39:49 -0600

    add errors

--- Modulo 8 API REST/alumnoapirest/src/main/java/mx/unam/dgtic/dto/AlumnoDto.java
+++ Modulo 8 API REST/alumnoapirest/src/main/java/mx/unam/dgtic/dto/AlumnoDto.java
@@ -14,10 +14,17 @@ public class AlumnoDto {
     @Min(value = 5)
     private String nombre;
 
+    @NotBlank(message = "No puede estar en blanco el apellido")
+    @Min(message = "El apellido debe ser de almenos 5 caracteres", value = 5)
     private String apellido;
 
+    @Email
+    @Min(value = 5 , message = "El correo debe tener al menos 10 caracteres")
     private String correo;
 
+    @Positive(message = "La calificacion debe ser positiva")
+    @Max(value = 10 , message = "La calificacion no puede ser mayor a 10")
+    @Min(value = 10 , message = "La calificacion no puede ser menor a 0")
     private Double promedio;
 
 }
--- Modulo 8 API REST/alumnoapirest/src/main/java/mx/unam/dgtic/exceptions/ManejadorGlobalDeExcepciones.java
+++ Modulo 8 API REST/alumnoapirest/src/main/java/mx/unam/dgtic/exceptions/ManejadorGlobalDeExcepciones.java
@@ -2,16 +2,34 @@ package mx.unam.dgtic.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ManejadorGlobalDeExcepciones {
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception ex){
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body ("Manejado por Controller Advice: " + ex.getMessage());
+//    @ExceptionHandler(Exception.class)
+//    public ResponseEntity<String> handleException(Exception ex){
+//        return ResponseEntity
+//                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                .body ("Manejado por Controller Advice: " + ex.getMessage());
+//    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, String>> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex){
+        Map<String, Object> respuesta = new HashMap<>();
+        Map<String, String> errorDetalle = new HashMap<>();
+        errorDetalle.put("mensaje", "El cuerpo esta mal formado");
+        errorDetalle.put("status", "" + HttpStatus.BAD_REQUEST);
+        Throwable causa =  ex.getMostSpecificCause();
+        if (causa != null){
+            errorDetalle.put("cause", causa.getMessage());
+        }
+
+        return ResponseEntity.badRequest().body(errorDetalle);
     }
 }
