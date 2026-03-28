10902c5b686ff1cbe932d6837164c76f90dd4ba2
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-03-28 09:36:46 -0600

    add bean WebClient

--- Modulo 8 API REST/alumnoapirest/src/main/java/mx/unam/dgtic/config/AppConfig.java
+++ Modulo 8 API REST/alumnoapirest/src/main/java/mx/unam/dgtic/config/AppConfig.java
@@ -4,6 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class AppConfig {
@@ -15,5 +16,15 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public WebClient webClient(){
+        // URL BASE del API REST Alumnos
+        String urlBase = "http://localhost:8080/api/v2/alumno";
+        return WebClient
+                .builder()
+                .baseUrl(urlBase)
+                .build();
     }
 }
