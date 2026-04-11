6a17269ecfde73da812a296479590075e059edce
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-04-10 20:34:57 -0600

    add dependencie lombok

--- Modulo 9 Spring Security/spring-security-101/src/main/java/edu/unam/controller/HelloWordController.java
+++ Modulo 9 Spring Security/spring-security-101/src/main/java/edu/unam/controller/HelloWordController.java
@@ -1,15 +1,18 @@
 package edu.unam.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 @RequestMapping("/auth")
 public class HelloWordController {
 
     @GetMapping("/welcome")
     public String helloWord(){
+        log.info("estoy en mi looooog!!");
         return "Hello Word";
     }
 }
