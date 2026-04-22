dd2008f4025337fafe00830b39b00a9cbd4d1af0
alonso1298 <alonsosagrerogranados@gmail.com>
2026-04-21 21:28:19 -0600

    add SecurityFilterChain

---
+++ Modulo 9 Spring Security/practica1/src/main/java/mx/una/dgtic/config/SecurityConfiguration.java
@@ -0,0 +1,42 @@
+package mx.una.dgtic.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests(auth -> auth
+                        // Recursos publicos
+                        .requestMatchers(
+                                "/login",
+                                "/bootstrap/**",
+                                "/tema/**",
+                                "/image/**",
+                                "/iconos/**"
+                        ).permitAll()
+                        // Pagina publica
+                        .requestMatchers("/").permitAll()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login") // login personalizado
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/", true) // direccion de donde va despues del login
+                        .failureUrl("/login?error=true")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout=true")
+                        .permitAll()
+                );
+        return http.build();
+    }
+}
