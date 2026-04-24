120fe87c22ae2c91e88a6114486b5231bbdd1814
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2026-04-24 16:53:56 -0600

    refactor

--- Modulo 9 Spring Security/spring-security-101/src/main/java/edu/unam/security/SecurityConfiguration.java
+++ Modulo 9 Spring Security/spring-security-101/src/main/java/edu/unam/security/SecurityConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
